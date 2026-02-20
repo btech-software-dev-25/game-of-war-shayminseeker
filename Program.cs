1daaf8a98362ec921d5ac6b4dcd0bea9a23aaa6d
Lucas Norton <128105773+shayminseeker@users.noreply.github.com>
2026-02-20 08:04:40 -0700

    Implement game logic in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
-﻿using GameOfWar;
+using GameOfWar;
 
 // Create an instance of the GameState class
 // Shuffle CardDeck within your instance
 // Deal 26 cards each from CardDeck to your instance's PlayerDeck and ComputerDeck
+GameState state = new GameState();
+state.CardDeck.Shuffle();
+
+state.PlayerDeck.PushCards(state.CardDeck.Deal(26));
+state.ComputerDeck.PushCards(state.CardDeck.Deal(26));
 
 
 // Create a function with the signature: static bool PlayCards(GameState state, int playerCardIndex)
@@ -16,33 +21,84 @@
 //     Check whether either state.PlayerDeck or state.ComputerDeck are empty
 //         If the computer deck is empty, the player wins and state.Winner should be set to "Computer"
 //         If the player deck is empty, the computer wins and state.Winner should be set to "Player"
+
 //     return true
+static bool PlayCards(GameState state, int playerCardIndex)
+{
+    Card playerCard = state.PlayerDeck.PullCardAtIndex(playerCardIndex);
+    Card computerCard = state.ComputerDeck.PullCardAtIndex(0);
+
+    if (playerCard > computerCard)
+    {
+        state.PlayerDeck.PushCard(playerCard);
+        state.PlayerDeck.PushCard(computerCard);
+        state.PlayerDeck.PushCards(state.TableDeck.PullAllCards());
+    }
+    else if (computerCard > playerCard)
+    {
+        state.ComputerDeck.PushCard(playerCard);
+        state.ComputerDeck.PushCard(computerCard);
+        state.ComputerDeck.PushCards(state.TableDeck.PullAllCards());
+    }
+    else
+    {
+        state.TableDeck.PushCard(playerCard);
+        state.TableDeck.PushCard(computerCard);
+    }
+
+    if (state.PlayerDeck.Count == 0)
+    {
+        state.Winner = "Computer";
+        return false;
+    }
+    else if (state.ComputerDeck.Count == 0)
+    {
+        state.Winner = "Player";
+        return false;
+    }
+
+    return true;
+}
 
 
 // Call Lib.RunGame(), passing two parameters: the state object you instantiated above and the name of your PlayCards function
+Lib.RunGame(state, PlayCards);
 
 namespace GameOfWar
 {
     public class GameState
     {
         // Create a public Deck property called CardDeck
+        public Deck CardDeck { get; set; }
 
 
         // Create a public Deck property called PlayerDeck
+        public Deck PlayerDeck { get; set; }
 
 
         // Create a public Deck property called ComputerDeck
+            public Deck ComputerDeck { get; set; }
 
 
         // Create a public Deck property called TableDeck
+        public Deck TableDeck { get; set; }
 
 
         // Create a public string property called Winner
+        public string Winner { get; set; }
 
 
         // Create a public constructor that accepts no parameters. It should:
         //    Initialize Winner to be empty (not null)
         //    Initialize CardDeck to be a new, fresh deck of 52 cards
         //    Initialize PlayerDeck, ComputerDeck, and TableDeck to be empty Deck objects (no cards)
+        public GameState()
+        {
+            Winner = string.Empty;
+            CardDeck = new Deck(null, false);
+            PlayerDeck = new Deck(null, true);
+            ComputerDeck = new Deck(null, true);
+            TableDeck = new Deck(null, true);
+        }
     }
 }
