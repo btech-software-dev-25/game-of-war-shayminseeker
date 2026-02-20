2163c2364e127c3d3040ec544274ccafb250d698
Lucas Norton <128105773+shayminseeker@users.noreply.github.com>
2026-02-20 08:03:51 -0700

    Update Card.cs

--- Card.cs
+++ Card.cs
@@ -1,60 +1,62 @@
-using System.Runtime.InteropServices.Swift;
-
 namespace GameOfWar
 {
     public class Card
     {
 
         // Create a string property Suit with a private setter
-        private string _suit;
-        public string Suit
-        {
-            get { return _suit; }
-
-            set { _suit = Suit; }
-        }
+        public string Suit { get; private set; }
 
 
         // Create an int property Rank with a private setter - values should range from 0 for a face value of 2 to 12 for an Ace
-        private int _rank;
-        public int Rank
-        {
+        public int Rank { get; private set; }
 
-            get { return _rank; }
-
-            set { _rank = Rank; }
-        }
 
 
         // Create a public constructor that takes suit and rank as arguments and assigns them to Suit and Rank
-
         public Card(string suit, int rank)
         {
-            _suit = suit;
-            _rank = rank;
+            Suit = suit;
+            Rank = rank;
         }
 
 
         // Overload the > operator to compare two cards by rank
-        public static void operator >(Card1, Card2) {
-            
+        public static bool operator >(Card card1, Card card2)
+        {
+            return card1.Rank > card2.Rank;
         }
 
 
-
-
         // Overload the < operator to compare two cards by rank
-        public static void operator <(Card1, Card2) {
-            
+        public static bool operator <(Card card1, Card card2)
+        {
+            return card1.Rank < card2.Rank;
         }
 
 
         // Create a public string method RankString that returns a string representation of this card's rank, 2-10 and Jack, Queen, King, Ace
         public string RankString()
         {
-            return $"{_rank} {_suit}";
+            if (Rank >= 0 && Rank <= 8)
+            {
+                return (Rank + 2).ToString();
+            }
+            else
+            {
+                switch (Rank)
+                {
+                    case 9:
+                        return "Jack";
+                    case 10:
+                        return "Queen";
+                    case 11:
+                        return "King";
+                    case 12:
+                        return "Ace";
+                    default:
+                        throw new InvalidOperationException("Invalid rank value.");
+                }
+            }
         }
-        
-        
     }
 }
